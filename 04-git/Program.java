66f7c5759caf4a45f7d54c57c783b4c247424fc5
unknown <student@lab-44-3.cs.put.poznan.pl>
2022-10-27 15:10:12 +0200

    Dodanie folderu 04-git a w nim projekt.java

---
+++ 04-git/Program.java
@@ -0,0 +1,28 @@
+public class B_Main {
+  int x = 88;
+
+  public static void main(String[] args) {
+    B_Main myObj = new B_Main();
+    System.out.println(myObj.x);
+  }
+}
+
+public class Class_Example 
+{
+    public Class_Example() 
+    {
+        String value = "Test";// Local Variable inside a Constructor
+    }
+
+    public void disp() 
+    {
+        String name = "JavaInterviewPoint";// Local variable inside method
+        System.out.println("Welcome my friend " + name + "!!!");
+    }
+}
+
+public class Main {
+
+  int x = 888899;
+
+}
