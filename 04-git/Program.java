8c6af69eb3f814048093a14c4cfb432c6facf10a
unknown <student@lab-44-3.cs.put.poznan.pl>
2022-10-27 15:49:31 +0200

    printuj x+-5 zamiast x

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -3,7 +3,8 @@ public class B_Main {
 
   public static void main(String[] args) {
     B_Main myObj = new B_Main();
-    System.out.println(myObj.x);
+    System.out.println(myObj.x - 5);
+    System.out.println(myObj.x + 5);
   }
 }
 
@@ -11,18 +12,22 @@ public class Class_Example
 {
     public Class_Example() 
     {
+      // stringi tego typu
         String value = "Test";// Local Variable inside a Constructor
+        String value = "OOOO";
     }
 
     public void disp() 
     {
+      // inne stringi 
         String name = "JavaInterviewPoint";// Local variable inside method
-        System.out.println("Welcome my friend " + name + "!!!");
+        System.out.println("Welcome my friend " + name + "!!!"
+          + "elo");
     }
 }
 
 public class Main {
 
   int x = 888899;
-
+  System.out.println(22+x);
 }
