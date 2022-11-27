5942c9cda559ba8f2b865b91711c3de163f4cad7
test <jakub.czarnecki.1@student.put.poznan.pl>
2022-11-27 22:32:18 +0100

    update 07-wzorce

---
+++ 07-wzorce/Patterns/src/main/java/put/io/patterns/implement/SystemCoolerObserver.java
@@ -0,0 +1,11 @@
+package put.io.patterns.implement;
+
+public class SystemCoolerObserver implements SystemStateObserver{
+    @Override
+    public void update(SystemState lastSystemState) {
+        // Increase CPU cooling if the temperature is too high
+        if (lastSystemState.getCpuTemp() > 60.00) {
+            System.out.println("> Increasing cooling of the CPU...");
+        }
+    }
+}
